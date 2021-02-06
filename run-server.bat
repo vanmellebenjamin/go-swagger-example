122c2cc30841ae0b7587c23e90bc302312f1038c
vanmellebenjamin <vanmelle.benjamin@gmail.com>
2021-02-07 00:22:35 +0100

    Improved Bat

--- run-server.bat
+++ run-server.bat
@@ -1,12 +1,17 @@
 @echo off
 
-cd server
-swagger generate server -A TodoList -f swagger.yaml
-cd ..
+swagger generate server -A TodoList -f server\swagger.yaml
+if not %errorlevel% == 0 (
+    echo Failed Code Generation
+    goto end
+)
 go test .\tests\...
 if %errorlevel% == 0 (
     go install .\server\cmd\todo-list-server\
     todo-list-server --port 8080 --tls-port 8085 --read-timeout 5s --tls-certificate C:\Users\vanme\go\src\flightAPI\openSSL\localhost.crt --tls-key C:\Users\vanme\go\src\flightAPI\openSSL\localhost.key
 ) else (
-    echo Unit tests Failed
+    echo Failed Unit tests
+    goto end
 )
+
+:End
