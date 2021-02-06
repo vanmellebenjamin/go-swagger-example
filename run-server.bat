0cdf7229cc44c38011173da8dbc95635c950a2f6
vanmellebenjamin <vanmelle.benjamin@gmail.com>
2021-02-07 00:24:55 +0100

    Improved Bat

--- run-server.bat
+++ run-server.bat
@@ -5,13 +5,19 @@ if not %errorlevel% == 0 (
     echo Failed Code Generation
     goto end
 )
+
 go test .\tests\...
-if %errorlevel% == 0 (
-    go install .\server\cmd\todo-list-server\
-    todo-list-server --port 8080 --tls-port 8085 --read-timeout 5s --tls-certificate C:\Users\vanme\go\src\flightAPI\openSSL\localhost.crt --tls-key C:\Users\vanme\go\src\flightAPI\openSSL\localhost.key
-) else (
-    echo Failed Unit tests
-    goto end
+if not %errorlevel% == 0 (
+   echo Failed Unit tests
+   goto end
 )
+
+go install .\server\cmd\todo-list-server\
+if not %errorlevel% == 0 (
+   echo Failed Install
+   goto end
+)
+
+todo-list-server --port 8080 --tls-port 8085 --read-timeout 5s --tls-certificate C:\Users\vanme\go\src\flightAPI\openSSL\localhost.crt --tls-key C:\Users\vanme\go\src\flightAPI\openSSL\localhost.key
 
 :End
